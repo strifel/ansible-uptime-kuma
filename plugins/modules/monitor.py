f1d66fe2b44da97299dab6fe84632bd349ca7a95
strifel <me@strifel.de>
2025-11-21 11:37:16 +0100

    Add IP-Family

--- plugins/modules/monitor.py
+++ plugins/modules/monitor.py
@@ -491,6 +491,7 @@ def main():
         oauth_client_secret=dict(type="str"),
         oauth_scopes=dict(type="str"),
         timeout=dict(type="int"),
+        ip_family=dict(type="str"),
 
         # KEYWORD
         keyword=dict(type="str"),
